f27b469d69f66d06b1c60322f8169db06f040eba
trunghv <gnurt250394@gmail.com>
2023-12-12 14:17:53 +0700

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const MyHomePage(
+        title: 'aaa',
+      ),
     );
   }
 }
